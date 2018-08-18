aa05cf26c852b1737a1c25e9382e5a8eb24aab07
Kazunobu Shimizu <kazu0goo@gmail.com>
2018-08-18 16:51:01 +0900

    props

--- src/App.js
+++ src/App.js
@@ -14,16 +14,38 @@ import React from 'react';
 // }
 
 const App = () => {
-  // return <div>HI!</div>
-  return <div>
-    <Cat />
-    <Cat />
-    <Cat />
-    <Cat />
-  </div>
+  const profiles = [
+    {
+      name:"Jiro"
+    },
+    {
+      name:"Taro",
+      age:11
+    },
+    {
+      name:"Hanako",
+      age:6
+    }
+  ]
+  return (
+    <div>
+      {
+        profiles.map((profile,index)=>{
+          return  <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
+    </div>
+  )
+  // return <div>
+  //   <User name={"Taro"} age={10}/>
+  //   <User name={"Hanako"} age={5}/>
+  // </div>
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>I am {props.name},age is {props.age}.</div>
+}
+User.defaultProps = {
+  age:1
 }
 export default App;
