37dd98b61e2cbfcd9606d2aa2602eacd00a8eab6
Kazunobu Shimizu <kazu0goo@gmail.com>
2018-08-18 16:23:50 +0900

    more jsx

--- src/App.js
+++ src/App.js
@@ -3,9 +3,30 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return (
-      <div><h1>Hello World!</h1></div>
+    // const greeting = "HI!"
+    // const dom = <h1 className="foo">{greeting} DOM</h1>;
+    // return dom;
+
+    //return <input type="text" onClick={()=>{console.log("WOOO! I'm clicked!")}} />;
+    //return <input type="text" onChange={()=>{console.log("WOOO! I'm changed!")}} />;
+    // return(
+    //   <div>
+    //     <label htmlFor="bar">bar</label>
+    //     <input type="text" onChange={()=>{console.log("WOOO! I'm changed!")}}></input>
+    //   </div>
+    // );
+    return(
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={()=>{console.log("WOOO! I'm changed!")}}></input>
+      </React.Fragment>
     );
+
+    //return React.createElement(
+    //   "div",
+    //   null,
+    //   "HELLO WORLD!"
+    // )
   }
 }
 
