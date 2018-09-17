11ddb27fa6022918c17058de787cc2d1a73f2e72
Kazunobu Shimizu <kazu0goo@gmail.com>
2018-09-17 19:37:54 +0900

    apply material-ui

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import EventsIndex from "./components/events_index";
 import EventsNew from "./components/events_new";
 import registerServiceWorker from "./registerServiceWorker";
 import { composeWithDevTools } from "redux-devtools-extension";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 const enhancer =
   process.env.NODE_ENV === "development"
@@ -19,17 +20,19 @@ const enhancer =
 const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/events/new" component={EventsNew} />
-        <Route path="/events/:id" component={EventsShow} />
-        <Route exact path="/events" component={EventsIndex} />
-        <Route exact path="/" component={EventsIndex} />
-      </Switch>
-    </BrowserRouter>
-    {/* <EventsIndex /> */}
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events/new" component={EventsNew} />
+          <Route path="/events/:id" component={EventsShow} />
+          <Route exact path="/events" component={EventsIndex} />
+          <Route exact path="/" component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
+      {/* <EventsIndex /> */}
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById("root")
 );
 registerServiceWorker();
